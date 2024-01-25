defa14496fa88869b87dcac0e07c4184a777c621
Ryan Blocker <62674519+ryancblocker@users.noreply.github.com>
2024-01-25 14:47:54 -0700

    add assignments

---
+++ assignments/repeating-patterns/main.py
@@ -0,0 +1,23 @@
+import math
+
+math.floor(2.5)
+
+# slice a list list[start:end] the result is all of the elements
+# in the list starting with list[start] and ending with list[end-1]
+
+# python supports negative indexing list[-1] which refers to the last element
+# and list[-2] refers to the second to last elememnt
+
+# the range() function typically used in a loop
+# the syntax is range(start,stop,increment). so to print the numbers from 0
+# up to but not including 10 it would be range(0,10,2)
+
+list1 = ['a','b','c','a','b','c']
+list2 = ['a','b','a','b','a','b']
+list3 = ['a','b','a','b','a','b', 'c']
+
+def repeat(list):
+    len_l = len(list) #length of the list
+    
+
+
