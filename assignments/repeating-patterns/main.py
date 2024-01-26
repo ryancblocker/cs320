f62fc49d23ddf9cc2fdc748bb4d97c7627d104ad
Ryan Blocker <62674519+ryancblocker@users.noreply.github.com>
2024-01-26 12:51:55 -0700

    notes

--- assignments/repeating-patterns/main.py
+++ assignments/repeating-patterns/main.py
@@ -1,23 +1,16 @@
-import math
+def repeats(lst):
+    if lst is None or len(lst) < 2:
+        return None
 
-math.floor(2.5)
-
-# slice a list list[start:end] the result is all of the elements
-# in the list starting with list[start] and ending with list[end-1]
-
-# python supports negative indexing list[-1] which refers to the last element
-# and list[-2] refers to the second to last elememnt
-
-# the range() function typically used in a loop
-# the syntax is range(start,stop,increment). so to print the numbers from 0
-# up to but not including 10 it would be range(0,10,2)
-
-list1 = ['a','b','c','a','b','c']
-list2 = ['a','b','a','b','a','b']
-list3 = ['a','b','a','b','a','b', 'c']
-
-def repeat(list):
-    len_l = len(list) #length of the list
-    
-
-
+    for pattern_length in range(1, len(lst) // 2 + 1):
+        if len(lst) % pattern_length == 0:
+            pattern = lst[:pattern_length]
+            if pattern * (len(lst) // pattern_length) == lst:
+                return pattern
+    return None
+# # Test cases
+# print(repeats(['a', 'b', 1, 'b', 'a', 'b', 1, 'b']))
+# print(repeats(['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b']))
+# print(repeats(['a', 'b', 'a', 'b', 'a', 'b', 'c']))
+# print(repeats([1]))
+# print(repeats(None))
