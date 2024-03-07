8c05e1a35e1f5caa66b4ef9e240f1bd01a63648b
Ryan Blocker <62674519+ryancblocker@users.noreply.github.com>
2024-03-07 16:15:30 -0700

    random-placement

--- assignments/random-placement/main.py
+++ assignments/random-placement/main.py
@@ -1,59 +1,40 @@
 import random
+
+
 def placement(num_objects, map):
-    avaliability_map = [[True] * len(map[0]) for i in range(len(map))]
+    if num_objects <= 0 or map is None:
+        return None
     
+    num_rows = len(map)
+    num_cols = len(map[0])
+
     def is_available(x, y):
-        if (avaliability_map[x][y]):
+        if (map[x][y]):
             return True
         else:
             return False
-
-    def find_open_spaces(map):
-        open_spaces = []
-        for x in range(0, len(map)):
-            for y in range(0, len(map)):
-                if (is_available(x, y)):
-                    open_spaces.append((x, y))
-        return open_spaces
-
-    def shuffle(open_spaces):
-        random_element = (0, 0)
-        for i in range(0, len(open_spaces)):
+    
+    open_spaces = []
+    for x in range(num_rows):
+        for y in range(num_cols):
+            if (is_available(x, y)):
+                open_spaces.append((x, y))
+    
+    free = len(open_spaces)
+    if num_objects > free:
+        return None
+    
+    def shuffle(spaces):
+        for i in range(len(spaces) - 1, -1, -1):
             if i == 0:
-                j = random.randint(0, len(open_spaces) - 1)
-            elif i == len(open_spaces) - 1:
-                j = random.randint(0, i - 1)
+                j = random.randint(0, len(spaces) - 1)
             else:
                 j = random.randint(0, i)
-            temp = open_spaces[j]
-            open_spaces[j] = open_spaces[i]
-            open_spaces[i] = temp
-            random_element = temp
-        return random_element
-
-    def select_spaces(num_objects, map):
-        objects_left = num_objects
-        random_spaces = []
-        while objects_left > 0:
-            open_spaces = find_open_spaces(map)
-            current_space = shuffle(open_spaces)    
-            avaliability_map[current_space[0]][current_space[1]] = False
-            random_spaces.append(current_space)
-            objects_left -= 1
-        print(avaliability_map)
-        return random_spaces
-            
-    if map is None:
-        return None
-    if not isinstance(map, list):
-        return None
-    if num_objects is None:
-        return None
-    if len(map) == 1 and len(map[0]) == 1:
-        return None
-    if not isinstance(num_objects, int):
-        return None
-    if num_objects > len(map) * len(map[0]):
-        return None
-
-    return select_spaces(num_objects, map)
+            temp = spaces[j]
+            spaces[j] = spaces[i]
+            spaces[i] = temp
+        return spaces
+    
+    shuffle(open_spaces)
+    
+    return open_spaces[:num_objects]
--- assignments/random-placement/test.py
+++ assignments/random-placement/test.py
@@ -1,7 +1,6 @@
 from main import placement
 
 print("2x2 Matrix Tests: ")
-print("\n")
 
 #test case with 2x2 map with 4 objects
 print("1. 2x2 World with 4 Objects: " + str(placement(4, [[(0,0), (0,1)], 
@@ -57,3 +56,62 @@ print("5. 3x6 World with 9 Objects: " + str(placement(9, [[(0,0), (0,1), (0,2), 
                                                           [(1,0), (1,1), (1,2), (1,3), (1,4), (1,5)], 
                                                           [(2,0), (2,1), (2,2), (2,3), (2,4), (2,5)]])))
 
+# import random
+# def placement(num_objects, map):
+
+#     if map is None:
+#         return None
+#     if num_objects is None:
+#         return None
+#     if not isinstance(map, list):
+#         return None
+#     if num_objects is None:
+#         return None
+#     if len(map) == 1 and len(map[0]) == 1:
+#         return None
+#     if not isinstance(num_objects, int):
+#         return None
+#     if num_objects > len(map) * len(map[0]):
+#         return None
+    
+#     avaliability_map = [[True] * len(map[0]) for i in range(len(map))]
+    
+#     def is_available(x, y):
+#         if (avaliability_map[x][y]):
+#             return True
+#         else:
+#             return False
+
+#     def find_open_spaces(map):
+#         open_spaces = []
+#         for x in range(0, len(map)):
+#             for y in range(0, len(map[0])):
+#                 if (is_available(x, y)):
+#                     open_spaces.append((x, y))
+#         return open_spaces
+
+#     def shuffle(open_spaces):
+#         random_element = (0, 0)
+#         for i in range(len(open_spaces) - 1, -1, -1):
+#             if i == 0:
+#                 j = random.randint(0, len(open_spaces) - 1)
+#             else:
+#                 j = random.randint(0, i)
+#             temp = open_spaces[j]
+#             open_spaces[j] = open_spaces[i]
+#             open_spaces[i] = temp
+#             random_element = temp
+#         return random_element
+
+#     def select_spaces(num_objects, map):
+#         objects_left = num_objects
+#         random_spaces = []
+#         while objects_left > 0:
+#             open_spaces = find_open_spaces(map)
+#             current_space = shuffle(open_spaces)    
+#             avaliability_map[current_space[0]][current_space[1]] = False
+#             random_spaces.append(current_space)
+#             objects_left -= 1
+#         return random_spaces
+
+#     return select_spaces(num_objects, map)
